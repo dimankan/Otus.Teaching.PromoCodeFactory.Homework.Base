ad6310e0492d3a4939d51be7a90084981a8ab8f2
dimankan <asus>
2024-03-18 21:17:51 +0300

    homework 1. 20240318

--- src/Otus.Teaching.PromoCodeFactory.Core/Domain/Administration/Employee.cs
+++ src/Otus.Teaching.PromoCodeFactory.Core/Domain/Administration/Employee.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using CSharpFunctionalExtensions;
+using System;
 using System.Collections.Generic;
+
 
 namespace Otus.Teaching.PromoCodeFactory.Core.Domain.Administration
 {
     public class Employee
         : BaseEntity
     {
+        public Employee()
+        {
+        }
+
+        private Employee(string firstName, string lastName, string email, List<Role> roles)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Roles = roles;
+        }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
@@ -16,5 +29,29 @@ namespace Otus.Teaching.PromoCodeFactory.Core.Domain.Administration
         public List<Role> Roles { get; set; }
 
         public int AppliedPromocodesCount { get; set; }
+
+        public static Result<Employee> Create(string firstName, string lastName, string email, List<Role> roles)
+        {
+            if (string.IsNullOrEmpty(firstName))
+            {
+                return Result.Failure<Employee>("Имя не может быть пустым");
+            }
+            if (string.IsNullOrEmpty(lastName))
+            {
+                return Result.Failure<Employee>("Фамилия не может быть пустой");
+            }
+            if (string.IsNullOrEmpty(email))
+            {
+                return Result.Failure<Employee>("Почта не может быть пустой");
+            }
+            if(roles.Count == 0)
+            {
+                return Result.Failure<Employee>("Роли не определены");
+            }
+
+            var employee = new Employee(firstName, lastName, email, roles);
+            return employee;
+        }
+
     }
 }
---
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Models/CreateEmployeeRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Otus.Teaching.PromoCodeFactory.WebHost.Models
+{
+    public class CreateEmployeeRequest
+    {
+        public CreateEmployeeRequest()
+        {
+            
+        }
+        public CreateEmployeeRequest(string firstName, string lastName, string email, List<string> roleNames)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            RoleNames = roleNames;
+        }
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public List<string> RoleNames { get; set; }
+    }
+}
